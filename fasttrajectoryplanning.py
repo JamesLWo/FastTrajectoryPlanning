b0cbd4432049ddd8e81c796fe71c9bd9c92eb895
rcp125 <rahul2000patel@gmail.com>
2020-02-13 19:05:51 -0500

    text output

--- fasttrajectoryplanning.py
+++ fasttrajectoryplanning.py
@@ -1,18 +1,21 @@
 import numpy as np 
 import random 
+np.set_printoptions(threshold=np.inf)
 
 #create actual maze and knowledge maze
-trueMaze = np.zeros(shape = (50,50))
-knowledgeMaze = np.zeros(shape = (50,50))
+trueMaze = np.zeros(shape = (50,50)).astype(int)
+knowledgeMaze = np.zeros(shape = (50,50)).astype(int)
 #populate actual maze
-for x in np.nditer(trueMaze):
+for x in np.nditer(trueMaze, op_flags=['readwrite']):
     randNum = random.randint(1,101)
     if randNum >= 1 and randNum <= 30:
         x = 1
     else:
-        x = 0 
-trueMaze[0,0] = 'S' 
-trueMaze[49,49] = 'G' 
+        x = 0
+trueMaze[0,0] = 1
+trueMaze[49,49] = 2
+
+np.savetxt('test.txt', trueMaze, delimiter=',', fmt='%.0f')
 
 print(trueMaze)
 
