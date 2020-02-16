751a419b2d5c91b0aca141e78fc7ac804cd97a14
James Wo <jameswo1234@gmail.com>
2020-02-16 15:57:35 -0500

    completed forward a star

--- forwardAStar.py
+++ forwardAStar.py
@@ -5,9 +5,11 @@ def forwardAStar(maze, beginningCoordinates, endingCoordinates):
     closedList = []
     beginningNode = Node(beginningCoordinates, None, 0, getManhattanDistance(beginningCoordinates, endingCoordinates))
     openList.append(beginningNode)
+    print("first item on open list: ")
+    print(openList[0].coordinates)
 
-    while openList[0].coordinates != endingCoordinates:
-        poppedNode = heapq.heappop(openList)
+    while openList[0].coordinates != endingCoordinates: #while first priority node isnt the goal node
+        poppedNode = heapq.heappop(openList) #remove first priority from open list and add to closed list and expand it
         closedList.append(poppedNode)
         #expand the popped node
         potentialCoordinates = []
@@ -17,21 +19,31 @@ def forwardAStar(maze, beginningCoordinates, endingCoordinates):
         potentialCoordinates.append(generateDownCoordinates(poppedNode.coordinates))
         for coordinate in potentialCoordinates:
             if isValidCoordinate(coordinate):
+                print("is Valid")
+                print(inClosed(coordinate, closedList))
+                print(inOpen(coordinate,closedList))
                 #if the neighbor has valid coordinates and is not in closed list or in the open list, add that node in the open list
                 if not(inClosed(coordinate, closedList)) and inOpen(coordinate, openList) == -1:
+                    print("is not in closed or open")
                     heapq.heappush(openList, Node(coordinate, poppedNode, poppedNode.gvalue + 1, getManhattanDistance(coordinate, endingCoordinates)))
-                #if the neighbor has valid cooridnates and is not in clased list but is in open list
+                #if the neighbor has valid cooridnates and is not in closed list but is in open list
                 elif not(inClosed(coordinate, closedList)) and inOpen(coordinate, openList) != -1:
                     #check if current distance can be improved
-                    if openList[inOpen(coordinate, openList)].fvalue > (poppedNode.gavlue + 1 + getManhattanDistance(coordinate, endingCoordinates)):
+                    if openList[inOpen(coordinate, openList)].fvalue > (poppedNode.gvalue + 1 + getManhattanDistance(coordinate, endingCoordinates)):
+                        #change fvalue, g value, h value, change parent
+                        openList[inOpen(coordinate, openList)].gvalue = poppedNode.gvalue + 1 
+                        openList[inOpen(coordinate, openList)].hvalue = getManhattanDistance(coordinate, endingCoordinates)
                         openList[inOpen(coordinate, openList)].fvalue = poppedNode.gvalue + 1 + getManhattanDistance(coordinate, endingCoordinates)
-
+                        openList[inOpen(coordinate, openList)].parent = poppedNode
+        print("next item on open list: ")
+        print(openList[0].coordinates)
     plannedPath = []
     goalNode = heapq.heappop(openList)
     currentNode = goalNode
     while currentNode !=  None:
-        plannedPath.insert(currentNode, 0)
+        plannedPath.append(currentNode.coordinates)
         currentNode = currentNode.parent
+    #return planned path
     return plannedPath
 def generateLeftCoordinates(currentCoordinates):
     return (currentCoordinates[0]-1, currentCoordinates[1])
@@ -46,13 +58,15 @@ def getManhattanDistance(currentCoordinates, endingCoordinates):
     return (endingCoordinates[0] - currentCoordinates[0]) + (endingCoordinates[1] - currentCoordinates[1])
 
 def isValidCoordinate(currentCoordinates):
-    return 0<=currentCoordinates[0]<=49 and 0<=currentCoordinates[1]<=49
+    return 0<=currentCoordinates[0]<=49 and 0<=currentCoordinates[1]<=49 and currentCoordinates != 1
+
 def inClosed(currentCoordinates, closedList):
     for node in closedList:
         if node.coordinates == currentCoordinates:
-            return False
+            return True
 
-    return True
+    return False
+
 def inOpen(currentCoordinates, openList):
     for index, w in enumerate(openList):
         if w.coordinates == currentCoordinates:
--- repeatedAStar.py
+++ repeatedAStar.py
@@ -1,23 +1,23 @@
 import forwardAStar
 
 def repeatedAStar(knowledgeMaze, trueMaze, beginningCoordinates, endingCoordinates):
-    plannedPaths = []
-    knowledgeMazes = []
+    #plannedPaths = []
+    #knowledgeMazes = []
 
-    currentKnowledgeMaze = knowledgeMaze
-    beginning = beginningCoordinates
-    ending = endingCoordinates
-    while True:
+    #currentKnowledgeMaze = knowledgeMaze
+    #beginning = beginningCoordinates
+    #ending = endingCoordinates
+    #while True:
         #planning
-        knowledgeMazes.append(currentKnowledgeMaze)
-        currentPath = forwardAStar.forwardAStar(knowledgeMaze, beginning, ending)
-        plannedPaths.append(currentPath)
+        #knowledgeMazes.append(currentKnowledgeMaze)
+        #currentPath = forwardAStar.forwardAStar(knowledgeMaze, beginning, ending)
+        #plannedPaths.append(currentPath)
         #execute
         #for each node in current path, step through and create updated knowledge maze
         #if fails, update beginning coordinates
         #call a star again with updated values
         #if finishes, break
-
+    return forwardAStar.forwardAStar(knowledgeMaze, beginningCoordinates, endingCoordinates)
 
 
 
