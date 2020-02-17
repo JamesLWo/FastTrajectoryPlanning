2b6e3960a485da627686731cc240c45fcbf5daf8
James Wo <jameswo1234@gmail.com>
2020-02-17 17:02:33 -0500

    Update fasttrajectoryplanning.py
    commit

--- fasttrajectoryplanning.py
+++ fasttrajectoryplanning.py
@@ -5,9 +5,10 @@ import adaptiveAStar
 import random 
 np.set_printoptions(threshold=np.inf)
 
+sizeOfGrid = 10
 #create actual maze and knowledge maze
-trueMaze = np.zeros(shape = (50,50)).astype(int)
-knowledgeMaze = np.zeros(shape = (50,50)).astype(int)
+trueMaze = np.zeros(shape = (sizeOfGrid,sizeOfGrid)).astype(int)
+knowledgeMaze = np.zeros(shape = (sizeOfGrid,sizeOfGrid)).astype(int)
 #populate actual maze
 for x in np.nditer(trueMaze, op_flags=['readwrite']):
     if random.random() >= 0.7:
@@ -15,15 +16,24 @@ for x in np.nditer(trueMaze, op_flags=['readwrite']):
     else:
         x[...] = 0
 trueMaze[0,0] = 3
-trueMaze[49,49] = 4
+trueMaze[sizeOfGrid-1,sizeOfGrid-1] = 4
 knowledgeMaze[0,0] = 3
-knowledgeMaze[49,49] = 4
+knowledgeMaze[sizeOfGrid-1,sizeOfGrid-1] = 4
+
+#give knowledge maze initial knowledge
+if trueMaze[1,0] == 1:
+    knowledgeMaze[1,0] = 1
+if trueMaze[0,1] == 1:
+    knowledgeMaze[0,1] = 1
 
 np.savetxt('test.txt', trueMaze, delimiter=',', fmt='%.0f')
 
+print("true maze: ")
 print(trueMaze)
 
-path = repeatedAStar.repeatedAStar(knowledgeMaze, trueMaze, (0,0), (49,49)) 
-path2 = repeatedBackwardsAStar.repeatedBackwardsAStar(knowledgeMaze, (0,0), (49,40))
-path3 = adaptiveAStar.adpativeAStar(knowledgeMaze, (0,0), (49,49))
+path = repeatedAStar.repeatedAStar(knowledgeMaze, trueMaze, (0,0), (sizeOfGrid-1,sizeOfGrid-1), sizeOfGrid) 
+print("answer: ")
+print(path)
+#path2 = repeatedBackwardsAStar.repeatedBackwardsAStar(knowledgeMaze, (0,0), (49,40))
+#path3 = adaptiveAStar.adpativeAStar(knowledgeMaze, (0,0), (49,49))
 
