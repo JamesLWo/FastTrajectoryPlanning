7c6cdf476787f8c69228880b0a0522a32c53ea85
rcp125 <rahul2000patel@gmail.com>
2020-02-18 19:51:31 -0500

    backwards and forwards test

--- fasttrajectoryplanning.py
+++ fasttrajectoryplanning.py
@@ -4,6 +4,8 @@ import repeatedBackwardsAStar
 import adaptiveAStar
 import random 
 import matplotlib.pyplot as plt
+from matplotlib import colors
+
 
 np.set_printoptions(threshold=np.inf)
 
@@ -17,6 +19,9 @@ for x in np.nditer(trueMaze, op_flags=['readwrite']):
         x[...] = 1
     else:
         x[...] = 0
+
+# trueMaze[0,1] = 1
+# trueMaze[1,0] = 1
 trueMaze[0,0] = 3
 trueMaze[sizeOfGrid-1,sizeOfGrid-1] = 4
 knowledgeMaze[0,0] = 3
@@ -33,13 +38,24 @@ np.savetxt('test.txt', trueMaze, delimiter=',', fmt='%.0f')
 print("true maze: ")
 print(trueMaze)
 
-path = repeatedAStar.repeatedAStar(knowledgeMaze, trueMaze, (0,0), (sizeOfGrid-1,sizeOfGrid-1), sizeOfGrid) 
+# path = repeatedAStar.repeatedAStar(knowledgeMaze, trueMaze, (0,0), (sizeOfGrid-1,sizeOfGrid-1), sizeOfGrid) 
+path = repeatedAStar.repeatedAStar(knowledgeMaze, trueMaze, (sizeOfGrid-1, sizeOfGrid-1), (0,0), sizeOfGrid)
 print("answer: ")
+# if path[0] == [[]]:
+#     print("no path")
+# else:
+#     print(path)
+
 print(path)
 #path2 = repeatedBackwardsAStar.repeatedBackwardsAStar(knowledgeMaze, (0,0), (49,40))
 #path3 = adaptiveAStar.adpativeAStar(knowledgeMaze, (0,0), (49,49))
-#plt.imshow(trueMaze)
-#lt.show()
+
+cmap = colors.ListedColormap(['white', 'black', 'yellow', 'red', 'green'])
+bounds=[0,1,2,3,4,5]
+norm = colors.BoundaryNorm(bounds, cmap.N)
+
+plt.imshow(trueMaze, cmap=cmap, norm=norm)
+plt.show()
 
 print(trueMaze)
 
