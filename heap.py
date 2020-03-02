518a1a5d62bf9d4a407312a707db0f1339bd1ad3
rcp125 <rahul2000patel@gmail.com>
2020-03-01 19:21:10 -0500

    comment

--- heap.py
+++ heap.py
@@ -1,39 +1,51 @@
 def heappush(heap, item):
     heap.append(item)
-    siftdown(heap, 0, len(heap)-1)
+    size = len(heap) - 1
+    siftdown(heap, 0, size)
 
 def heappop(heap):
     last = heap.pop()
     if heap:
-        ret = heap[0]
+        retval = heap[0]
         heap[0] = last
         siftup(heap, 0)
-        return ret
+        return retval
     return last
  
-def siftdown(heap, startpos, pos):
-    newitem = heap[pos]
-    while pos > startpos:
-        parentpos = (pos - 1) >> 1
+def siftdown(heap, start, index):
+    insert = heap[index]
+    while index > start:
+        parentpos = get_parent(index)
         parent = heap[parentpos]
-        if newitem < parent:
-            heap[pos] = parent
-            pos = parentpos
+        if insert < parent:
+            heap[index] = parent
+            index = parentpos
             continue
         break
-    heap[pos] = newitem
+    heap[index] = insert
 
-def siftup(heap, pos):
-    endpos = len(heap)
-    startpos = pos
-    newitem = heap[pos]
-    childpos = 2*pos + 1
-    while childpos < endpos:
-        rightpos = childpos + 1
-        if rightpos < endpos and not heap[childpos] < heap[rightpos]:
-            childpos = rightpos
-        heap[pos] = heap[childpos]
-        pos = childpos
-        childpos = 2*pos + 1
-    heap[pos] = newitem
-    siftdown(heap, startpos, pos)
+def siftup(heap, index):
+    end = len(heap)
+    start = index
+    insert = heap[index]
+    child = get_child(index)
+    while child < end:
+        right = child + 1
+        if right < end and not heap[child] < heap[right]:
+            child = right
+        heap[index] = heap[child]
+        index = child
+        child = get_child(index)
+    heap[index] = insert
+    siftdown(heap, start, index)
+
+def get_child(index):
+    return 2*index + 1
+
+def get_parent(index):
+    return (index - 1)/2
+
+''' 
+python heapq library implementation courtesy of cpython library
+customized and removed functions for own heap class for purpose of fast trajectory replanning
+'''
